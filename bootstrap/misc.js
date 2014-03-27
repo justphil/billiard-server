f6432a9ef838f914e893a6d4a0e60e8a5c541a86
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2014-03-27 22:36:57 +0100

    remove some debug logs

--- bootstrap/misc.js
+++ bootstrap/misc.js
@@ -5,8 +5,6 @@ const express = require('express');
 module.exports = function(expressLogLevel, pathToStaticResources) {
     var app = express();
 
-    console.log('pathToStaticResources ->', pathToStaticResources);
-
     app.use(express.logger(expressLogLevel));
     app.use(express.static(pathToStaticResources));
 
